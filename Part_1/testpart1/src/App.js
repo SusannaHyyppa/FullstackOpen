8b97767b834ec448cdd359ede3f47bb962d197f3
Susanna <sussu.hyyppa@gmail.com>
2023-04-27 11:43:51 +0300

    some parts

--- Part_1/testpart1/src/App.js
+++ Part_1/testpart1/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Hello = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const friends = [ 'Leevi', 'Venla']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+
+export default App
